fdffc067f770473679b612d895422673d951abfe
Zach-Adolphsen <zladolphsen@gmail.com>
2025-03-06 12:24:03 -0600

    Added Inputs to the HousingLocationComponent

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { HousingLocation } from '../housing-location';
 
 @Component({
   selector: 'app-housing-location',
@@ -6,4 +8,6 @@ import { Component } from '@angular/core';
   templateUrl: './housing-location.component.html',
   styleUrl: './housing-location.component.css',
 })
-export class HousingLocationComponent {}
+export class HousingLocationComponent {
+  @Input() housingLocation!: HousingLocation;
+}
